6a7ab51dcab48a624f58d1a1fa1907bcdeef5cfe
Olesia Ladanai <olesia.ladanai@symphony-solutions.com>
2024-04-10 17:14:02 +0300

    pysanka
    add pysanka

--- src/App.js
+++ src/App.js
@@ -1,26 +1,9 @@
-import React, { useState } from 'react';
-import './App.css';
-import MindArViewer from './components/MindArViewer';
+import React from 'react';
 
 function App() {
-  const [started, setStarted] = useState(null);
 
   return (
-    <div className="App">
-      <h1>AR Scanner</h1>
-
-      <div className="control-buttons">
-        {started === null && <button onClick={() => {setStarted('aframe')}}>Start AFRAME version</button>}
-        {started !== null && <button onClick={() => {setStarted(null)}}>Stop</button>}
-      </div>
-
-      {started === 'aframe' && (
-        <div className="container">
-          <MindArViewer />
-          <video></video>
-        </div>
-      )}
-    </div>
+    <></>
   );
 }
 
