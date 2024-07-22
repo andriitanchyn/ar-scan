5e63c17611b8c9107932327f55c6ba3e80ba6830
Olesia Ladanai <olesia.ladanai@symphony-solutions.com>
2024-07-22 19:04:24 +0300

    add cave model

--- src/mindar-viewer.js
+++ src/mindar-viewer.js
@@ -25,11 +25,12 @@ const MindAr = () => {
 
   return (
     <>
-      <a-scene inspector="url: http://localhost:3333/dist/aframe-inspector.js" ref={sceneRef} mindar-image={`imageTargetSrc: targets3.mind; filterBeta: 0; autoStart: false; uiLoading: no; uiError: no; uiScanning: yes;`} color-space="sRGB" embedded renderer="colorManagement: true, physicallyCorrectLights" vr-mode-ui="enabled: false" device-orientation-permission-ui="enabled: false">
+      <a-scene inspector="url: http://localhost:3333/dist/aframe-inspector.js" ref={sceneRef} mindar-image={`imageTargetSrc: targets4.mind; filterBeta: 0; autoStart: false; uiLoading: no; uiError: no; uiScanning: yes;`} color-space="sRGB" embedded renderer="colorManagement: true, physicallyCorrectLights" vr-mode-ui="enabled: false" device-orientation-permission-ui="enabled: false">
         <a-assets>
           <a-asset-item id="pysankaModel" src="pysanka_v2-04.gltf"></a-asset-item>
           <a-asset-item id="clayModel" src="Clay_tablet.gltf"></a-asset-item>
           <a-asset-item id="flopyModel" src="Floppy_disc.gltf"></a-asset-item>
+          <a-asset-item id="caveModel" src="Cave_painting.gltf"></a-asset-item>
         </a-assets>
 
         <a-camera position="0 0 0"></a-camera>
@@ -42,6 +43,9 @@ const MindAr = () => {
         </a-entity>
         <a-entity mindar-image-target="targetIndex: 2">
           <a-gltf-model rotation="0 0 0"scale="0.1 0.1 0.1" position="0 -0.4 0" src="#flopyModel" animation-mixer="clip: *; loop: repeat; timeScale: 1"></a-gltf-model>
+        </a-entity>
+        <a-entity mindar-image-target="targetIndex: 3">
+          <a-gltf-model rotation="0 0 0" position="0 -0.1 0" src="#caveModel" animation-mixer="clip: *; loop: repeat; timeScale: 1"></a-gltf-model>
         </a-entity>
       </a-scene>
     </>
